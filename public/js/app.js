90a6974f9821689f637cecd283f397ad872b9be8
moises <moisesheredia1@gmail.com>
2017-11-16 02:21:44 -0400

    Cambios
    Configuracion de Tabs con Angular

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,24 @@
 	var app = angular.module('equipos',[]);
 
 	app.controller('EquipoController', function(){
+		 this.tab = 1;
 		this.equipo={
 			id:'001',
 			name:'Brasil',
 			continente:'Sudamerica',
 			fundacion:'1914',
 			tecnico:'Tite (2016)',
-			ranking:['Octubre 2017','2do-']
+			ranking:['Octubre 2017','2do-'],
+			stats: {
+		        attack: 99,
+		        defense: 90,
+		    },
+		    copas: [ "alemania 2006", "Brasil 1985", "Usa 1993" ]
 		};
+
+		 this.selectTab = function (tab) {
+	      	this.tab = tab;
+	     };
 
 	});
 
